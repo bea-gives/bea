b4b8e4146f9cc69c3e08daf3921c76817482ff41
Damien Mortini <ddotmnn+git@gmail.com>
2021-02-17 19:53:12 +0100

    bea-font: fix font weight

--- packages/bea-font/index.js
+++ packages/bea-font/index.js
@@ -2,6 +2,6 @@ document.fonts.add(new FontFace('Mulish', 'url(node_modules/@bea-org/bea-font/mu
 
 document.fonts.add(new FontFace('Pangram', 'url(node_modules/@bea-org/bea-font/pangram-regular.woff2)'));
 
-document.fonts.add(new FontFace('Pangram', 'url(node_modules/@bea-org/bea-font/pangram-bold.woff2)'), { weight: 700 });
+document.fonts.add(new FontFace('Pangram', 'url(node_modules/@bea-org/bea-font/pangram-bold.woff2)', { weight: 700 }));
 
-document.fonts.add(new FontFace('Pangram', 'url(node_modules/@bea-org/bea-font/pangram-extrabold.woff2)'), { weight: 800 });
+document.fonts.add(new FontFace('Pangram', 'url(node_modules/@bea-org/bea-font/pangram-extrabold.woff2)', { weight: 800 }));
