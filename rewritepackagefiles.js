988286af8e45c5eab39273d0ffb9bd04a139bcb0
Damien Mortini <ddotmnn+git@gmail.com>
2021-02-17 18:16:14 +0100

    add rewritepackagefiles script and update packages

---
+++ rewritepackagefiles.js
@@ -0,0 +1,27 @@
+import fs from 'fs';
+import fastGlob from 'fast-glob';
+
+// Re-write package.json
+for (const result of fastGlob.sync([
+  '*/package.json',
+  'element/*/package.json',
+], {
+  cwd: 'packages',
+  objectMode: true,
+})) {
+  const filePath = `packages/${result.path}`;
+  const directory = filePath.replace(`/${result.name}`, '');
+  const packageData = {
+    ...JSON.parse(fs.readFileSync(filePath, 'utf-8')),
+    author: 'Béa',
+    license: 'ISC',
+    repository: {
+      type: 'git',
+      url: 'https://github.com/bea-org/bea',
+      directory,
+    },
+    bugs: 'https://github.com/bea-org/bea/issues',
+    homepage: `https://github.com/bea-org/bea/tree/master/${directory}`,
+  };
+  fs.writeFileSync(filePath, `${JSON.stringify(packageData, null, 2)}\n`);
+}
